84033aa2f88aaad8a3dafb881fae653b7f4101fd
raketavlad <zerosworld@mail.ru>
2024-02-23 22:40:37 +0300

    Шестая корректировка финального ДЗ второго урока

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -10,36 +10,39 @@ public class GuessNumber {
         this.player2 = player2;
     }
 
-    public static int generateRandomNumber() {
-        return 1 + (int) (Math.random() * 100);
-    }
-
     public void play() {
-        int randomNumber = generateRandomNumber();
+        int targetNumber = generateTargetNumber();
         while (true) {
-            if (enterAndCheckNumber(player1, randomNumber)) {
+            enterNumber(player1);
+            if (checkNumber(player1, targetNumber)) {
                 break;
             }
-            if (enterAndCheckNumber(player2, randomNumber)) {
+            enterNumber(player2);
+            if (checkNumber(player2, targetNumber)) {
                 break;
             }
         }
     }
 
-    public boolean enterAndCheckNumber(Player player, int randomNumber) {
+    private static int generateTargetNumber() {
+        return 1 + (int) (Math.random() * 100);
+    }
+
+    private void enterNumber(Player player) {
         Scanner sc = new Scanner(System.in);
         System.out.print(player.getName() + " введите число: ");
         player.setNumber(sc.nextInt());
-        sc.nextLine();
-        if (player.getNumber() > randomNumber) {
+    }
+
+    private boolean checkNumber(Player player, int targetNumber) {
+        if (player.getNumber() > targetNumber) {
             System.out.println("Число " + player.getNumber() +
                     " больше того, что загадал компьютер!");
-        } else if (player.getNumber() < randomNumber) {
+        } else if (player.getNumber() < targetNumber) {
             System.out.println("Число " + player.getNumber() +
                     " меньше того, что загадал компьютер!");
         } else {
-            System.out.println("Игрок " + player.getName() + " победил!");
-            System.out.println();
+            System.out.println("Игрок " + player.getName() + " победил!\n");
             return true;
         }
         return false;
