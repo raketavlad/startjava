e625c20b69028329410315a845298bb9a1fdeb6b
raketavlad <zerosworld@mail.ru>
2024-03-20 21:25:54 +0300

    "Первое исправление калькулятора из 4-го урока"

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -4,43 +4,39 @@ public class Calculator {
 
     private int num1;
     private int num2;
-    private String sign;
+    private char sign;
 
-    public int getNum1() {
-        return num1;
-    }
-
-    public int getNum2() {
-        return num2;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public void setParams(String parameters) {
-        String[] userExpression = parameters.split(" ");
-        num1 = Integer.parseInt(userExpression[0]);
-        num2 = Integer.parseInt(userExpression[2]);
-        sign = userExpression[1];
-    }
-
-    public double calculate() {
+    public double calculate(String parameters) {
+        String[] elements = parameters.split(" ");
+        num1 = Integer.parseInt(elements[0]);
+        num2 = Integer.parseInt(elements[2]);
+        sign = elements[1].charAt(0);
         switch (sign) {
-            case "+":
+            case '+':
                 return num1 + num2;
-            case "-":
+            case '-':
                 return num1 - num2;
-            case "*":
+            case '*':
                 return num1 * num2;
-            case "/":
-                return (double) num1 / (double) num2;
-            case "^":
+            case '/':
+                return (double) num1 / num2;
+            case '^':
                 return Math.pow(num1, num2);
-            case "%":
+            case '%':
                 return num1 % num2;
             default:
+                System.out.println("Ошибка: знак " + sign + " не поддерживается");
                 return Double.NaN;
+        }
+    }
+
+    public void printResult(double result) {
+        String expression = "Результат вычисления равен: " + num1 + " " + sign + " " + num2 + " = ";
+        if (Double.isNaN(result)) {
+        } else if (result % 1 == 0) {
+            System.out.printf("%s%.0f\n", expression, result);
+        } else {
+            System.out.printf("%s%.3f\n", expression, result);
         }
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -7,28 +7,14 @@ public class CalculatorTest {
         Scanner sc = new Scanner(System.in);
         Calculator calc = new Calculator();
         String continuation = "yes";
-        while (continuation.equals("yes") || !continuation.equals("no")) {
+        while (!continuation.equals("no")) {
             if (continuation.equals("yes")) {
                 System.out.print("Введите математическое выражение: ");
-                calc.setParams(sc.nextLine());
-                double result = calc.calculate();
-                if (Double.isNaN(result)) {
-                    System.out.println("Ошибка: знак " + calc.getSign() + " не поддерживается");
-                } else {
-                    System.out.print("Результат вычисления равен: " + calc.getNum1() + " " + calc.getSign() + " " +
-                            calc.getNum2() + " = ");
-                    if (result % 1 == 0) {
-                        System.out.printf("%.0f\n", result);
-                    } else {
-                        System.out.printf("%.3f\n", result);
-                    }
-                }
+                double result = calc.calculate(sc.nextLine());
+                calc.printResult(result);
             }
             System.out.print("Хотите продолжить вычисления? ");
             continuation = sc.nextLine();
-            if (continuation.equals("no")) {
-                break;
-            }
         }
     }
 }
