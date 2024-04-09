7d617a957443b961e39768c08dc4d9a7bb2da627
raketavlad <zerosworld@mail.ru>
2024-04-09 21:28:00 +0300

    "Первое исправление задания по SQL"

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -5,18 +5,24 @@ CREATE DATABASE robots;
 \c robots;
 
 --Создать таблицу
-CREATE TABLE jaegers (id SERIAL PRIMARY KEY,
-                      model_name TEXT,
-                      mark TEXT,
-                      height REAL,
-                      weight REAL,
-                      status TEXT,
-                      origin TEXT,
-                      launch DATE,
-                      kaiju_kill INTEGER);
+CREATE TABLE jaegers (
+    id SERIAL  PRIMARY KEY,
+    model_name VARCHAR(30),
+    mark       VARCHAR(6),
+    height     REAL,
+    weight     DECIMAL,
+    status     VARCHAR(30),
+    origin     VARCHAR(30),
+    launch     DATE,
+    kaiju_kill INTEGER
+);
 
 -- Вызов скрипта для заполнения таблицы данными
 \i init_db.sql
 
 -- Вызов скрипта с запросами
 \i queries.sql
+
+-- Удаление таблицы
+\c postgres
+DROP DATABASE robots;
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,28 +1,53 @@
 \echo 'Вывод всей таблицы'
-SELECT * FROM jaegers ORDER BY model_name;
+SELECT *
+  FROM jaegers
+ ORDER BY model_name;
 
 \echo 'Вывод всей информации про не уничтоженных Егерей'
-SELECT * FROM jaegers WHERE status != 'Destroyed' ORDER BY model_name;
+SELECT *
+  FROM jaegers
+ WHERE status != 'Destroyed'
+ ORDER BY model_name;
 
 \echo 'Вывод всей информации про Егерей первого и четвертого поколения'
-SELECT * FROM jaegers WHERE mark IN ('Mark-1', 'Mark-4') ORDER BY model_name;
+SELECT *
+  FROM jaegers
+ WHERE mark IN ('Mark-1', 'Mark-4')
+ ORDER BY model_name;
 
 \echo 'Вывод всей информации про всех Егерей кроме первого и четвертого поколения'
-SELECT * FROM jaegers WHERE mark NOT IN ('Mark-1', 'Mark-4') ORDER BY mark DESC;
+SELECT *
+  FROM jaegers
+ WHERE mark NOT IN ('Mark-1', 'Mark-4')
+ ORDER BY mark DESC;
 
 \echo 'Вывод всей информации про самого старого Егеря'
-SELECT * FROM jaegers ORDER BY launch LIMIT 1;
+SELECT *
+  FROM jaegers
+ ORDER BY launch LIMIT 1;
 
 \echo 'Вывод названия, марки, года выпуска и количество убитых Кайдзю для трех самых результативных Егерей'
-SELECT model_name, mark, launch, kaiju_kill FROM jaegers ORDER BY kaiju_kill DESC LIMIT 3;
+SELECT model_name, mark, launch, kaiju_kill
+  FROM jaegers
+ ORDER BY kaiju_kill DESC LIMIT 3;
 
 \echo 'Вывод среднего веса всех Егерей, округленного до трех знаков после запятой'
-SELECT ROUND(CAST(AVG(weight) AS DECIMAL), 3) AS avg_weigth FROM jaegers;
+SELECT ROUND(AVG(weight), 3) AS average_weight
+  FROM jaegers;
 
 \echo 'Вывод всей информации про Егерей, при этом увеличив количество убитых Кайдзю на 1 для ещё живых Егерей'
-UPDATE jaegers SET kaiju_kill = kaiju_kill + 1 WHERE status != 'Destroyed';
-SELECT * FROM jaegers ORDER BY model_name;
+UPDATE jaegers
+   SET kaiju_kill = kaiju_kill + 1
+ WHERE status != 'Destroyed';
+
+SELECT *
+  FROM jaegers
+ ORDER BY model_name;
 
 \echo 'Удаление из таблицы всех уничтоженных Егерей и вывод всей информации про оставшихся'
-DELETE FROM jaegers WHERE status = 'Destroyed';
-SELECT * FROM jaegers;
+DELETE FROM jaegers
+ WHERE status = 'Destroyed';
+
+SELECT *
+  FROM jaegers
+ ORDER BY model_name;
